ba4d5b4bb2871980e156eb050f8a1ea4136d180b
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-03-17 14:36:32 +0200

    add

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -4,7 +4,28 @@ function Footer() {
 
     return (
         <>
-        
+            <footer>
+                <div className={styles.footerContainer}>
+                    <br />
+                    
+                    <div className={styles.mediaContainer}>
+                        <a href="tel:+19548736034">
+                            <img src="/media/phone.png" alt="" />
+                        </a>
+                        <a href="https://t.me/NoProblemAuto" target="_blank" rel="noopener noreferrer">
+                            <img src="/media/telegram.jpg" alt="" />
+                        </a>
+                        <a href="https://www.instagram.com/noproblem.auto/" target="_blank" rel="noopener noreferrer">
+                            <img src="/media/instagram.jpg" alt="" />
+                        </a>
+                        <a href="https://www.facebook.com/profile.php?id=61572476408682&sk=photos" target="_blank" rel="noopener noreferrer">
+                            <img src="/media/facebook.jpg" alt="" />
+                        </a>
+                    </div>
+
+                    <br />
+                </div>
+            </footer>
         </>
     )
 }
