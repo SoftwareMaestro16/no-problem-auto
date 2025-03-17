808882d2827820af35ef615bc77968930543b860
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-03-17 11:03:49 +0200

    add

--- src/Main/App.jsx
+++ src/Main/App.jsx
@@ -57,16 +57,6 @@ function App() {
               {t("slogan")}
             </motion.h2>
 
-            {/* <motion.button 
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.1, delay: 0.1 }}
-            >
-              {t("get-disc")}
-            </motion.button> */}
-
             <motion.button 
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
